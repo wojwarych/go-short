49645e352d7651fb16b3e55f67d03aca73e55f71
wojwarych <woj.warych@gmail.com>
2020-08-27 09:59:21 +0200

    add simple example of creating new record to DB via POST, creating db and showing records

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/gin-gonic/gin"
 
+	"github.com/warycwoj/go-short/db"
 	"github.com/warycwoj/go-short/shortener"
 )
 
@@ -16,15 +17,27 @@ type LongURL struct {
 func main() {
 	r := gin.Default()
 	r.LoadHTMLGlob("templates/*")
+	if _, err := db.Open(); err != nil {
+		panic(err)
+	}
+	if !db.DB.HasTable(&URL{}){
+		db.DB.AutoMigrate(&URL{})
+	}
+	defer db.Close()
 	r.GET("/", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "index.tmpl", gin.H{
 			"title": "Main website",
 		})
+		urls := db.DB.Find(&URL{})
+		log.Println(urls)
 	})
 	r.POST("/", func(c *gin.Context) {
 		var longURL LongURL
 		c.ShouldBind(&longURL)
-		log.Printf("%v", shortener.Shorten(12345))
+		shortURL := shortener.Shorten(12345)
+		newURL := &URL{LongURL: longURL.LongURL, ShortURL: shortURL}
+		db.DB.Create(newURL)
+		log.Printf("%s, %v", "Successfully added new URL to DB", *newURL)
 	})
 	r.Run(":8000")
 }
