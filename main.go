dbeaf12ca25e3a4820eaa65ef619a36ed10ef3a9
wojwarych <woj.warych@gmail.com>
2020-08-31 00:06:36 +0200

    added wiping DB on startup; basic flow of POST url to DB with error handling; basic flow of redirect from short url to source

--- main.go
+++ main.go
@@ -1,16 +1,19 @@
 package main
 
 import (
+	"errors"
 	"log"
 	"net/http"
+	"net/url"
 
 	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
 
 	"github.com/warycwoj/go-short/db"
 	"github.com/warycwoj/go-short/shortener"
 )
 
-type LongURL struct {
+type PostedURL struct {
 	LongURL string `form:"longURL"`
 }
 
@@ -21,7 +24,11 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	db.AutoMigrate(&URL{})
+	db.Migrator().DropTable(&URL{})
+	db.Migrator().CreateTable(&URL{})
+	if db.Migrator().HasTable(&URL{}) {
+		db.Migrator().AutoMigrate(&URL{})
+	}
 	r.GET("/", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "index.tmpl", gin.H{
 			"title": "Main website",
@@ -30,16 +37,44 @@ func main() {
 		for rows.Next() {
 			var url URL
 			db.ScanRows(rows, &url)
-			log.Printf("%v\n", url)
+			log.Printf("%v\n, %d", url, url.ID)
 		}
 	})
 	r.POST("/", func(c *gin.Context) {
-		var longURL LongURL
-		c.ShouldBind(&longURL)
-		shortURL := shortener.Shorten(12345)
-		newURL := &URL{LongURL: longURL.LongURL, ShortURL: shortURL}
-		db.Create(newURL)
-		log.Printf("%s, %v", "Successfully added new URL to DB", *newURL)
+		var postedURL PostedURL
+		c.ShouldBind(&postedURL)
+		lastRecord := URL{}
+		ret := db.Last(&lastRecord)
+		var shortPath string
+		if errors.Is(ret.Error, gorm.ErrRecordNotFound) {
+			shortPath = shortener.Shorten(1)
+		} else {
+			shortPath = shortener.Shorten(lastRecord.ID + 1)
+		}
+		_, err := url.Parse(postedURL.LongURL)
+		if err != nil {
+			panic(err)
+		}
+		short := url.URL{
+			Scheme: "http",
+			Host:   "localhost:8000",
+			Path:   shortPath,
+		}
+		log.Printf("%s", short)
+		newURLRow := &URL{LongURL: postedURL.LongURL, ShortURL: short.String()}
+		db.Create(newURLRow)
+	})
+	r.GET("/:url/", func(c *gin.Context) {
+		desiredURL := c.Param("url")
+		decodedPK := shortener.Decoder(desiredURL)
+		urlModel := URL{}
+		ret := db.First(&urlModel, decodedPK)
+		if errors.Is(ret.Error, gorm.ErrRecordNotFound) {
+			c.HTML(http.StatusNotFound, "URL not found in DB!", nil)
+		} else {
+			c.Redirect(http.StatusFound, urlModel.LongURL)
+			db.Model(&urlModel).Update("visits", urlModel.Visits+1)
+		}
 	})
 	r.Run(":8000")
 }
